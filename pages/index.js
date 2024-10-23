7fe27806ac4504d88a80d569669953c963ee6d53
gianttelepathicsquid <kevinxrp@gmail.com>
2024-10-22 21:14:21 -0700

    Delete pages directory

--- pages/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { OnboardingGuide } from '../components/OnboardingGuide';  // Remove .jsx extension
-
-export default function Home() {
-  return <OnboardingGuide />;
-}
